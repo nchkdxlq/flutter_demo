00aa8b989c6de2798b0389145d8b5a85e2c47d50
luoquan <nchkdxlq@gmail.com>
2020-05-20 20:17:42 +0800

    Scaffold 组件

--- lib/Container/ScaffoldRoute.dart
+++ lib/Container/ScaffoldRoute.dart
@@ -29,27 +29,33 @@ class _ScaffoldRouteState extends State<ScaffoldRoute> with SingleTickerProvider
 				actions: <Widget>[ // 导航栏右侧菜单
 					IconButton(icon: Icon(Icons.share), onPressed: () => {},)
 				],
-//				bottom: TabBar(
-//					controller: _tabController,
-//					tabs: tabs.map( (e) => Tab(text: e) ).toList()
-//				),
+				bottom: TabBar(
+					controller: _tabController,
+					tabs: tabs.map( (e) => Tab(text: e) ).toList()
+				),
 			),
-			bottomNavigationBar: BottomNavigationBar( // 底部导航
-				items: <BottomNavigationBarItem>[
-					BottomNavigationBarItem(icon: Icon(Icons.home), title: Text('Home')),
-					BottomNavigationBarItem(icon: Icon(Icons.business), title: Text('Business')),
-					BottomNavigationBarItem(icon: Icon(Icons.school), title: Text('School')),
-				],
-				currentIndex: _selectedIndex,
-				fixedColor: Colors.blue,
-				onTap: _onItemTapped,
-			),
+//			drawer: myDrawer(),
+			body: body(context),
+			bottomNavigationBar: bottomAppBar(context),
+//			bottomNavigationBar: bottomNavigationBar(context),
 			floatingActionButton: FloatingActionButton( // 悬浮按钮
 				child: Icon(Icons.add),
 				onPressed: _onAdd,
 			),
 		);
   }
+
+  Widget myDrawer() {
+		return Builder(builder: (context) {
+			return IconButton(
+				icon: Icon(Icons.dashboard, color: Colors.white,),
+				onPressed: () {
+					// 打开抽屉菜单
+					Scaffold.of(context).openDrawer();
+				},
+			);
+		});
+	}
 
 
   void _onItemTapped(int index) {
@@ -60,5 +66,46 @@ class _ScaffoldRouteState extends State<ScaffoldRoute> with SingleTickerProvider
 
 	void _onAdd() {
 
+	}
+
+	Widget body(BuildContext context) {
+		return TabBarView(
+			controller: _tabController,
+			children: tabs.map( (e) {
+				return Container(
+					alignment: Alignment.center,
+					child: Text(e, textScaleFactor: 5,),
+				);
+			} ).toList(),
+		);
+	}
+
+
+	BottomNavigationBar bottomNavigationBar(BuildContext context) {
+		return BottomNavigationBar( // 底部导航
+			items: <BottomNavigationBarItem>[
+				BottomNavigationBarItem(icon: Icon(Icons.home), title: Text('Home')),
+				BottomNavigationBarItem(icon: Icon(Icons.business), title: Text('Business')),
+				BottomNavigationBarItem(icon: Icon(Icons.school), title: Text('School')),
+			],
+			currentIndex: _selectedIndex,
+			fixedColor: Colors.blue,
+			onTap: _onItemTapped,
+		);
+	}
+
+	BottomAppBar bottomAppBar(BuildContext context) {
+		return BottomAppBar(
+			color: Colors.white,
+			shape: CircularNotchedRectangle(),
+			child: Row(
+				mainAxisAlignment: MainAxisAlignment.spaceAround,
+				children: <Widget>[
+					IconButton(icon: Icon(Icons.home),),
+					SizedBox(),
+					IconButton(icon: Icon(Icons.business),)
+				],
+			),
+		);
 	}
 }
